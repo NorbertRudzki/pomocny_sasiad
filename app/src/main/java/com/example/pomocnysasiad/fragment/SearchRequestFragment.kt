958cd13f31b1b2d673338c9138f9a5400cbdc003
unknown <piotr1q2w@gmail.com>
2020-12-10 20:52:31 +0100

    test-mapy

--- app/src/main/java/com/example/pomocnysasiad/fragment/SearchRequestFragment.kt
+++ app/src/main/java/com/example/pomocnysasiad/fragment/SearchRequestFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import com.example.pomocnysasiad.R
+import com.example.pomocnysasiad.model.LocationService
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.fragment_search_request.*
 
 
-class SearchRequestFragment : Fragment() {
+class SearchRequestFragment : Fragment(), OnMapReadyCallback {
+
+    var mMap: GoogleMap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +27,26 @@ class SearchRequestFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search_request, container, false)
     }
+
+    override fun onStart() {
+        super.onStart()
+        val mapFragment = mapView as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        readLocation.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                locationDisplay.text = "current location:\nlatitude = ${LocationService.myLocation.latitude} \nlongitude = ${LocationService.myLocation.longitude}"
+                mMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(LocationService.myLocation.latitude,LocationService.myLocation.longitude)))
+            }
+        })
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap
+        mMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(LocationService.myLocation.latitude,LocationService.myLocation.longitude)))
+
+    }
+
+
 
 }
--- app/src/main/java/com/example/pomocnysasiad/model/LocationService.kt
+++ app/src/main/java/com/example/pomocnysasiad/model/LocationService.kt
@@ -1,4 +1,40 @@
 package com.example.pomocnysasiad.model
 
-class LocationService {
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.core.content.ContextCompat
+import android.os.Bundle
+
+object LocationService  {
+    var myLocation: Location = Location("")
+
+    @SuppressLint("MissingPermission")
+    fun initialize(locationManager : LocationManager) {
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                5000,
+                0f,
+                object : LocationListener {
+                    override fun onLocationChanged(loc: Location) {
+                        myLocation = loc;
+                    }
+
+                    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+                        //TODO("Not yet implemented")
+                    }
+
+                    override fun onProviderEnabled(p0: String?) {
+                        //TODO("Not yet implemented")
+                    }
+
+                    override fun onProviderDisabled(p0: String?) {
+                        //TODO("Not yet implemented")
+                    }
+                }
+        )
+    }
 }
