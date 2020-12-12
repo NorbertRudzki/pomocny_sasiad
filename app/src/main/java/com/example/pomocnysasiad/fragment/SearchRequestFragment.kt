828c6731cefe62c07cc71d54c002768bf9754db7
Norbert <norudzki@gmail.com>
2020-12-12 23:21:42 +0100

    fixed search request map

--- app/src/main/java/com/example/pomocnysasiad/fragment/SearchRequestFragment.kt
+++ app/src/main/java/com/example/pomocnysasiad/fragment/SearchRequestFragment.kt
@@ -8,10 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import com.example.pomocnysasiad.R
 import com.example.pomocnysasiad.model.LocationService
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.fragment_search_request.*
 
@@ -28,24 +25,35 @@ class SearchRequestFragment : Fragment(), OnMapReadyCallback {
         return inflater.inflate(R.layout.fragment_search_request, container, false)
     }
 
-    override fun onStart() {
-        super.onStart()
-        val mapFragment = mapView as SupportMapFragment
-        mapFragment.getMapAsync(this)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        MapsInitializer.initialize(requireActivity().applicationContext)
+        mapView.getMapAsync(this)
 
-        readLocation.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                locationDisplay.text = "current location:\nlatitude = ${LocationService.myLocation.latitude} \nlongitude = ${LocationService.myLocation.longitude}"
-                mMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(LocationService.myLocation.latitude,LocationService.myLocation.longitude)))
-            }
-        })
+
+        readLocation.setOnClickListener {
+            mMap?.setMinZoomPreference(8f)
+            locationDisplay.text =
+                "current location:\nlatitude = ${LocationService.myLocation.latitude} \nlongitude = ${LocationService.myLocation.longitude}"
+            mMap?.moveCamera(
+                CameraUpdateFactory.newLatLng(
+                    LatLng(
+                        LocationService.myLocation.latitude,
+                        LocationService.myLocation.longitude
+                    )
+                )
+            )
+
+        }
     }
 
-    override fun onMapReady(googleMap: GoogleMap?) {
-        mMap = googleMap
-        mMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(LocationService.myLocation.latitude,LocationService.myLocation.longitude)))
 
-    }
+   override fun onMapReady(googleMap: GoogleMap?) {
+       mMap = googleMap
+       mMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(LocationService.myLocation.latitude,LocationService.myLocation.longitude)))
+   }
 
 
 
