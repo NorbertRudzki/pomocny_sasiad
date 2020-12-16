64a7fdf2969ad54b519d876621661a8b3e42fc9a
Norbert <norudzki@gmail.com>
2020-12-16 23:55:13 +0100

    added get requests from firestore

--- app/src/main/java/com/example/pomocnysasiad/fragment/SearchRequestFragment.kt
+++ app/src/main/java/com/example/pomocnysasiad/fragment/SearchRequestFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.pomocnysasiad.R
 import com.example.pomocnysasiad.model.LocationService
 import com.example.pomocnysasiad.model.Request
@@ -19,18 +22,18 @@ import kotlinx.android.synthetic.main.fragment_search_request.*
 
 
 class SearchRequestFragment : Fragment(), OnMapReadyCallback {
-
+    private val requestViewModel by viewModels<RequestViewModel>()
     var mMap: GoogleMap? = null
     var currentRequests: List<Request>? = listOf(
-            Request(1, "", "", "testN", "", "", GeoPoint(52.0, 19.0), 0),
-            Request(2, "", "", "testE", "", "", GeoPoint(51.5, 19.5), 0),
-            Request(3, "", "", "testS", "", "", GeoPoint(51.0, 19.0), 0),
-            Request(4, "", "", "testW", "", "", GeoPoint(51.5, 18.5), 0)
+        Request(1, "", "", "testN", "", "", GeoPoint(52.0, 19.0), 0),
+        Request(2, "", "", "testE", "", "", GeoPoint(51.5, 19.5), 0),
+        Request(3, "", "", "testS", "", "", GeoPoint(51.0, 19.0), 0),
+        Request(4, "", "", "testW", "", "", GeoPoint(51.5, 18.5), 0)
     )
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search_request, container, false)
@@ -44,24 +47,37 @@ class SearchRequestFragment : Fragment(), OnMapReadyCallback {
         mapView.getMapAsync(this)
 
         readLocation.setOnClickListener { refreshMap() }
+        requestViewModel.getAllRequests().observe(viewLifecycleOwner) {
+            if (it != null) {
+                currentRequests = it
+                refreshMap()
+            }
+        }
+
+      //  todo w taki sposob przechodzi się do innego fragmentu i przekazuje wartość w bundle jako mapa key - value
+        //todo a tak się odbiera:   val value = arguments?.get("key")
+     //   findNavController().navigate(
+     //       SearchRequestFragmentDirections.actionSearchRequestFragment2ToRequestDetails().actionId,
+     //       bundleOf("key" to "value")
+     //   )
     }
 
-    fun refreshMap(){
+    fun refreshMap() {
         mMap?.setMinZoomPreference(8f)
         locationDisplay.text =
-                "current location:\nlatitude = ${LocationService.myLocation.latitude} \nlongitude = ${LocationService.myLocation.longitude}"
+            "current location:\nlatitude = ${LocationService.myLocation.latitude} \nlongitude = ${LocationService.myLocation.longitude}"
         mMap?.moveCamera(
-                CameraUpdateFactory.newLatLng(
-                        LatLng(
-                                LocationService.myLocation.latitude,
-                                LocationService.myLocation.longitude
-                        )
+            CameraUpdateFactory.newLatLng(
+                LatLng(
+                    LocationService.myLocation.latitude,
+                    LocationService.myLocation.longitude
                 )
+            )
         )
         mMap?.addMarker(
-                MarkerOptions()
-                        .position(LatLng(51.0, 19.0))
-                        .title("My test pin")
+            MarkerOptions()
+                .position(LatLng(51.0, 19.0))
+                .title("My test pin")
         )
         currentRequests?.let {
             showPins(it)
@@ -70,7 +86,14 @@ class SearchRequestFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap?) {
         mMap = googleMap
-        mMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(LocationService.myLocation.latitude, LocationService.myLocation.longitude)))
+        mMap?.moveCamera(
+            CameraUpdateFactory.newLatLng(
+                LatLng(
+                    LocationService.myLocation.latitude,
+                    LocationService.myLocation.longitude
+                )
+            )
+        )
         currentRequests?.let {
             showPins(it)
         }
@@ -80,9 +103,9 @@ class SearchRequestFragment : Fragment(), OnMapReadyCallback {
         mMap?.let {
             for (req in requests) {
                 it.addMarker(
-                        MarkerOptions()
-                                .position(LatLng(req.location.latitude, req.location.longitude))
-                                .title(req.title)
+                    MarkerOptions()
+                        .position(LatLng(req.location.latitude, req.location.longitude))
+                        .title(req.title)
                 )
             }
 
