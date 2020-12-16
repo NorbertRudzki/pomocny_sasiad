17d5c2490e979199a62aacfeb070b3ebc8738bcf
unknown <piotr1q2w@gmail.com>
2020-12-16 23:35:27 +0100

    plain pins

--- app/src/main/java/com/example/pomocnysasiad/fragment/SearchRequestFragment.kt
+++ app/src/main/java/com/example/pomocnysasiad/fragment/SearchRequestFragment.kt
@@ -1,5 +1,6 @@
 package com.example.pomocnysasiad.fragment
 
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,18 +9,28 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import com.example.pomocnysasiad.R
 import com.example.pomocnysasiad.model.LocationService
+import com.example.pomocnysasiad.model.Request
+import com.example.pomocnysasiad.viewmodel.RequestViewModel
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.GeoPoint
 import kotlinx.android.synthetic.main.fragment_search_request.*
 
 
 class SearchRequestFragment : Fragment(), OnMapReadyCallback {
 
     var mMap: GoogleMap? = null
+    var currentRequests: List<Request>? = listOf(
+            Request(1, "", "", "testN", "", "", GeoPoint(52.0, 19.0), 0),
+            Request(2, "", "", "testE", "", "", GeoPoint(51.5, 19.5), 0),
+            Request(3, "", "", "testS", "", "", GeoPoint(51.0, 19.0), 0),
+            Request(4, "", "", "testW", "", "", GeoPoint(51.5, 18.5), 0)
+    )
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search_request, container, false)
@@ -32,29 +43,51 @@ class SearchRequestFragment : Fragment(), OnMapReadyCallback {
         MapsInitializer.initialize(requireActivity().applicationContext)
         mapView.getMapAsync(this)
 
+        readLocation.setOnClickListener { refreshMap() }
+    }
 
-        readLocation.setOnClickListener {
-            mMap?.setMinZoomPreference(8f)
-            locationDisplay.text =
+    fun refreshMap(){
+        mMap?.setMinZoomPreference(8f)
+        locationDisplay.text =
                 "current location:\nlatitude = ${LocationService.myLocation.latitude} \nlongitude = ${LocationService.myLocation.longitude}"
-            mMap?.moveCamera(
+        mMap?.moveCamera(
                 CameraUpdateFactory.newLatLng(
-                    LatLng(
-                        LocationService.myLocation.latitude,
-                        LocationService.myLocation.longitude
-                    )
+                        LatLng(
+                                LocationService.myLocation.latitude,
+                                LocationService.myLocation.longitude
+                        )
                 )
-            )
-
+        )
+        mMap?.addMarker(
+                MarkerOptions()
+                        .position(LatLng(51.0, 19.0))
+                        .title("My test pin")
+        )
+        currentRequests?.let {
+            showPins(it)
         }
     }
 
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap
+        mMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(LocationService.myLocation.latitude, LocationService.myLocation.longitude)))
+        currentRequests?.let {
+            showPins(it)
+        }
+    }
 
-   override fun onMapReady(googleMap: GoogleMap?) {
-       mMap = googleMap
-       mMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(LocationService.myLocation.latitude,LocationService.myLocation.longitude)))
-   }
+    fun showPins(requests: List<Request>) {
+        mMap?.let {
+            for (req in requests) {
+                it.addMarker(
+                        MarkerOptions()
+                                .position(LatLng(req.location.latitude, req.location.longitude))
+                                .title(req.title)
+                )
+            }
 
+        }
+    }
 
 
 }
